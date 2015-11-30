c8de29041e042b9284e44c9cb010b8e6e62a44d8
rohit29 <rbishnoi209@gmail.com>
2015-12-01 01:40:56 +0530

    updated

--- java/basic/com/first/MainActivity.java
+++ java/basic/com/first/MainActivity.java
@@ -1,4 +1,7 @@
 package basic.com.first;
+import java.util.Timer;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
@@ -20,7 +23,7 @@ class preclass extends Thread
 {
     MainActivity th=new MainActivity();
     Client user;//=new Client("localhost", 45353, response);
-
+    long TIMEMAX = 1000000000;
     public preclass(MainActivity th1)
     {
             th = th1 ;
@@ -30,20 +33,62 @@ class preclass extends Thread
     public void run()
     {
          user.rec = true;
-            while (!user.updated) {
+        long starttime = System.nanoTime() ;
+        starttime /= TIMEMAX ;
+        while(!user.updated){
+                long   currenttime =  System.nanoTime()  ;
+                   currenttime /= TIMEMAX;
+            if(currenttime - starttime > 10 )break ;
+        }
+        if(user.updated) {
+            user.updated = false;
 
-            }
-        user.updated=false;
+            final int rr;
+            if(user.recval != -2)
+            rr = Parameters.clicked(user.recval, th.tu2);
+            else rr = -2 ;
+            th.gridview.post(new Runnable() {
+                public void run() {
+                    th.current = th.tu2 ;
+                    th.makechanges(rr);
+                    if (rr == -2) {
+                        if (th.tu1 == 1) {
+                            Toast.makeText(th, "Player 2 win (LATE Player 1 )!" + th.score,
+                                    Toast.LENGTH_SHORT).show();
+                            th.score = 0;
+                        } else {
+                            Toast.makeText(th, "Player 1 win (LATE Player 2 )!" + th.score,
+                                    Toast.LENGTH_SHORT).show();
+                            th.score = 0;
+                        }
+                        // th.gridview.setAdapter(new ImageAdapter(th.a));
+                    }
+                }
+            });
+        }
+        else {
+            user.sendval = -2 ;
+            user.send= true;
+            th.gridview.post(new Runnable() {
+                public void run() {
+                    th.current = th.tu1;
+                    th.makechanges(-2);
+                    if(th.tu1 == 1) {
+                        Toast.makeText(th, "Player 1 win (LATE Player 2 )!",
+                                Toast.LENGTH_SHORT).show();
+                        th.score = 0;
 
-           final int rr ;
-            rr = Parameters.clicked(user.recval,th.tu2);
+                    }
+                    else {
+                        Toast.makeText(th, "Player 2 win (LATE Player 1 )!",
+                                Toast.LENGTH_SHORT).show();
+                        th.score = 0;
+                    }
+                    // th.gridview.setAdapter(new ImageAdapter(th.a));
+                }
+            });
+        }
 
-        th.gridview.post(new Runnable() {
-            public void run() {
-                th.makechanges(rr);
-                // th.gridview.setAdapter(new ImageAdapter(th.a));
-            }
-        });
 //
 
     } 
@@ -51,10 +96,10 @@ class preclass extends Thread
 
 public class MainActivity extends AppCompatActivity {
     public boolean check_player2=false;
-
     public  int tu1 = 1;
         public  int tu2;
         Client user;
+    int current = 0;
         public  int score = 0 ;
         String response;
         public GridView gridview;
@@ -62,12 +107,24 @@ public class MainActivity extends AppCompatActivity {
         int rr;
         MainActivity th = this ;
         public void makechanges(int rr) {
-            if (rr != -1) {
+            if (rr != -1 && rr != -2) {
                 score++;
                 gridview.setAdapter(new ImageAdapter(a));
             } else if (rr == 0) {
-             //   Toast.makeText(MainActivity.this, "Game Over!"+score  ,
-             //         Toast.LENGTH_SHORT).show();score=0;
+                if(current == 1) {
+                    Toast.makeText(MainActivity.this, "Player 1 wins !(Cleared Player 2)",
+                            Toast.LENGTH_SHORT).show();
+                    score = 0;
+                }
+                else {
+                    Toast.makeText(MainActivity.this, "Player 2 wins !(Cleared Player 1)",
+                            Toast.LENGTH_SHORT).show();
+                    score = 0;
+                }
+                }
+            else if(rr == -2){
+                Parameters.reset();
+                gridview.setAdapter(new ImageAdapter(a));
             }
             if (score > 2) {
                 boolean test = Parameters.checkEnd();
@@ -106,21 +163,27 @@ public class MainActivity extends AppCompatActivity {
                 }
                 user.updated = false;
                 rr = Parameters.clicked(user.recval, tu2);
+                current = tu2 ;
                 makechanges(rr);
             }
             gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                 public void onItemClick(AdapterView<?> parent, View v,
                                         int position, long id) {
-                    while (user.rec) {
 
+                    if (!user.rec) {
+                        rr = Parameters.clicked(position, tu1);
+                        if(rr !=-1) {
+
+                            current = tu1 ;
+                            makechanges(rr);
+                            user.sendval = position;
+                            user.send = true;
+
+                            preclass play2 = new preclass(th);
+                            //  play2.preclass1(th) ;
+                            play2.start();
+                        }
                     }
-                    rr = Parameters.clicked(position, tu1);
-                    makechanges(rr);
-                    user.sendval = position;
-                    user.send = true;
-                    preclass play2=new preclass(th);
-                  //  play2.preclass1(th) ;
-                    play2.start(); 
                 }
             });
         }
